63a8b44efb0a83ca39ffffeb449482da2115092a
Fisherman <aflee@wisc.edu>
2023-09-23 19:22:52 -0500

    First Round of Code, Bare Minimum

---
+++ questionaire.js
@@ -0,0 +1,14 @@
+var questionaire = [
+    
+"What is the process of finding errors and fixing them within a program?",
+"Debugging",
+
+"What is the function that allows you to print something to the console?",
+"console.log()",
+
+"What is the function that converts an integer to a string?",
+"toString()",
+
+"What is the syntax for to calculate remainder?",
+"%"];
+
