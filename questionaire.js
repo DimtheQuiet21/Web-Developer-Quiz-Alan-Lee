be4954b657ca658530aa20a90c209c3d666f9547
Fisherman <aflee@wisc.edu>
2023-09-27 13:14:51 -0500

    Adding new questions and pulling out the notebook

--- questionaire.js
+++ questionaire.js
@@ -10,5 +10,23 @@ var questionaire = [
 "toString()",
 
 "What is the syntax for to calculate remainder?",
-"%"];
+"%",
+
+"What javascript DOM command changes the css style of an element?",
+".setAttribute()",
+
+"How do you create a new element using only javascript?",
+"document.createElement()",
+
+"How do you store an item into the User computer's memory?",
+"localStorage.setItem()",
+
+"When you are storing an array to the User computer's memory, what must you call?",
+"JSON",
+
+"How do you add elements to other elements using javascript?",
+".appendChild()",
+
+"How do you prevent a submit button from refreshing the page on clicking?",
+".preventDefault()",];
 
